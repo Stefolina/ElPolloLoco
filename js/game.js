97983ba79dd0a0f15476d1b6ef396981b4ca7791
Stefanie Nader <stefanie.nader1992@web.de>
2021-05-10 23:53:05 +0200

    third commit

--- js/game.js
+++ js/game.js
@@ -1,11 +1,11 @@
 let canvas;
-let character = new Image();
 let ctx;
+let character = new character();
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    character.src = '../img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png';
 
-    ctx.drawImage(character,20,20,50,150);
+    console.log('My charackter is ', character)
+    
 }
