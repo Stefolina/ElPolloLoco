7be8a72ffde9f374b4186fa132d3cef4a8197d8c
Stefanie Nader <stefanie.nader1992@web.de>
2021-05-12 23:21:57 +0200

    alles bewegt sich

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -2,10 +2,18 @@ class Cloud extends MovableObject {
     y = 10;
     width = 400;
     height = 350;
+    speed = 0.15;
 
     constructor(){
         super().loadImage('img/5.Fondo/Capas/4.nubes/1.png');
     
         this.x = Math.random() * 500; //Zahl zwischen 200 und 700
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            this.x -= this.speed;
+        }, 1000/ 60);
     }
 }
